e0ed9a31d2c0290c79518f0126db667d63d3f932
Gitlab_account <bia029@campus.mephi.ru>
2026-05-17 01:01:26 +0300

    change allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,8 +13,8 @@ origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
